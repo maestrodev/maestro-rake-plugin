3be9c2ca80ae755c0fd255bace8ce334cd2b591c
Carlos Sanchez <csanchez@maestrodev.com>
2013-08-20 10:41:21 -0700

    Refactor to use rspec let

--- spec/rake_worker_spec.rb
+++ spec/rake_worker_spec.rb
@@ -19,44 +19,46 @@ require 'spec_helper'
 
 describe MaestroDev::Plugin::RakeWorker do
 
-  before(:all) do
-    Maestro::MaestroWorker.mock!
-    @ruby_version = ENV['RUBY_VERSION']
-
+  let(:ruby_version) { ENV['RUBY_VERSION'] }
+  let(:rubygems_version) do
     rg = Maestro::Util::Shell.run_command("rvm #{@ruby_version} do gem --version")
-    @rubygems_version = rg[0].success? ? rg[1].chomp : ''
+    rg[0].success? ? rg[1].chomp : ''
   end
+
+  before do
+    Maestro::MaestroWorker.mock!
+    subject.perform(:execute, workitem)
+  end
+
+  let(:fields) {{}}
+  let(:workitem) {{ 'fields' => fields }}
 
   describe 'valid_workitem?' do
     it "should validate fields" do
-      workitem = {'fields' =>{}}
-
-      subject.perform(:execute, workitem)
-
       workitem['fields']['__error__'].should include('missing field path')
     end
 
-    it "should detect rake, rvm, bundle not available" do
-      workitem = {'fields' => {
+    context "when rake, rvm, bundle not available" do
+      let(:fields) {{
         'tasks' => '', 
-         'path' => '/tmp',
-         'rake_executable' => '/dev/nul',
-         'rvm_executable' => '/dev/nul',
-         'bundle_executable' => '/dev/nul', 
-         'use_rvm' => true,
-         'use_bundle' => true
+        'path' => '/tmp',
+        'rake_executable' => '/dev/nul',
+        'rvm_executable' => '/dev/nul',
+        'bundle_executable' => '/dev/nul', 
+        'use_rvm' => true,
+        'use_bundle' => true
       }}
 
-      subject.perform(:execute, workitem)
-
-      workitem['fields']['__error__'].should include("rake not installed")
-      workitem['fields']['__error__'].should include("rvm not installed")
-      workitem['fields']['__error__'].should include("bundle not installed")
-      workitem['fields']['__error__'].should include("missing ruby_version")
+      it "should detect" do
+        workitem['fields']['__error__'].should include("rake not installed")
+        workitem['fields']['__error__'].should include("rvm not installed")
+        workitem['fields']['__error__'].should include("bundle not installed")
+        workitem['fields']['__error__'].should include("missing ruby_version")
+      end
     end
-  
-    it "should validate rvm fields" do
-      workitem = {'fields' => {
+
+    context "when using rvm" do
+      let(:fields) {{
         'tasks' => '',
         'path' => '/tmp', 
         'use_rvm' => true,
@@ -64,67 +66,69 @@ describe MaestroDev::Plugin::RakeWorker do
         'rubygems_version' => '99.99.99'
       }}
 
-      subject.perform(:execute, workitem)
-
-      workitem['fields']['__error__'].should include("Requested Ruby version")
-      workitem['fields']['__error__'].should include("Requested RubyGems version")
+      it "should validate rvm fields" do
+        workitem['fields']['__error__'].should include("Requested Ruby version")
+        workitem['fields']['__error__'].should include("Requested RubyGems version")
+      end
     end
-  
-    it "should validate with only scm path" do
-      workitem = {'fields' => {
-         'rake_executable' => 'echo 1',
-         'scm_path' => '/tmp'
+
+    context "when using scm path" do
+      let(:fields) {{
+        'rake_executable' => 'echo 1',
+        'scm_path' => '/tmp'
       }}
 
-      subject.perform(:execute, workitem)
-
-      workitem['__output__'].should include('1')
-      workitem['fields']['__error__'].should be_nil
+      it "should validate with only scm path" do
+        workitem['__output__'].should include('1')
+        workitem['fields']['__error__'].should be_nil
+      end
     end
   end
 
   describe 'execute' do
-    before :each do
-      @path = File.join(File.dirname(__FILE__), '..') 
-      @workitem =  {'fields' => {'tasks' => '',
-         'path' => @path,
-         'use_rvm' => true,
-         'ruby_version' => @ruby_version,
-         'rubygems_version' => @rubygems_version,
-         'use_bundle' => true}}
-    end
+    let(:path) { File.join(File.dirname(__FILE__), '..') }
+    let(:fields) {{
+      'tasks' => '',
+      'path' => path,
+      'use_rvm' => true,
+      'ruby_version' => ruby_version,
+      'rubygems_version' => rubygems_version,
+      'use_bundle' => true
+    }}
 
     # Problem in that it tries to run bundler outside of rvm
 #    it 'should run rake with bundler' do
-#      @workitem['fields']['tasks'] = '--version'
-#      @workitem['fields']['use_rvm'] = false
+#      workitem['fields']['tasks'] = '--version'
+#      workitem['fields']['use_rvm'] = false
 #
-#      subject.perform(:execute, @workitem)
+#      subject.perform(:execute, workitem)
 #
-#      puts @workitem
-#      @workitem['fields']['__error__'].should be_nil
-#      @workitem['fields']['output'].should include("rake, version")
-#      @workitem['fields']['output'].should_not include("ERROR")
+#      puts workitem
+#      workitem['fields']['__error__'].should be_nil
+#      workitem['fields']['output'].should include("rake, version")
+#      workitem['fields']['output'].should_not include("ERROR")
 #    end
-    
-    it 'should run rake with rvm' do
-      @workitem['fields']['tasks'] = '--version'
-      @workitem['fields']['use_bundle'] = false
 
-      subject.perform(:execute, @workitem)
+    context "when running rake with rvm" do
+      let(:fields) { super().merge({
+        'tasks' => '--version',
+        'use_bundle' => false
+      }) }
 
-      @workitem['fields']['__error__'].should be_nil
-      @workitem['__output__'].should include("rake, version")
-      @workitem['__output__'].should_not include("ERROR")
+      it 'should run rake with rvm' do
+        workitem['fields']['__error__'].should be_nil
+        workitem['__output__'].should include("rake, version")
+        workitem['__output__'].should_not include("ERROR")
+      end
     end
 
     # This whole thing is running non-mri
 #    it 'should run rake with rvm using non mri ruby' do
-#      @workitem['fields']['tasks'] = '--version'
-#      @workitem['fields']['environment'] = 'CC=/usr/bin/gcc-4.2'
-#      @workitem['fields']['ruby_version'] = 'jruby-1.6.4'
-#      @workitem['fields']['rubygems_version'] = '1.8.6'
-#      @workitem['fields']['use_bundle'] = false
+#      workitem['fields']['tasks'] = '--version'
+#      workitem['fields']['environment'] = 'CC=/usr/bin/gcc-4.2'
+#      workitem['fields']['ruby_version'] = 'jruby-1.6.4'
+#      workitem['fields']['rubygems_version'] = '1.8.6'
+#      workitem['fields']['use_bundle'] = false
 #         
 #         @test_participant.stubs(:update_ruby => [Maestro::Shell::ExitCode.new(0), "all clear"])
 #         @test_participant.stubs(:validate_output => 'using /some/path/jruby-1.6.4  rake, version 0.9.2')
@@ -135,32 +139,36 @@ describe MaestroDev::Plugin::RakeWorker do
 #      workitem['fields']['output'].should_not include("ERROR")
 #    end
 
-    it 'should run rake with rvm and bundler' do
-      @workitem['fields']['tasks'] = '--version'
-      @workitem['fields']['environment'] = 'CC=/usr/bin/gcc-4.2'
+    context "when running rake with rvm and bundler" do
+      let(:fields) { super().merge({
+        'tasks' => '--version',
+        'environment' => 'CC=/usr/bin/gcc-4.2'
+      }) }
 
-      subject.perform(:execute, @workitem)
-
-      @workitem['fields']['__error__'].should be_nil
-      @workitem['__output__'].should include("rake, version")
-      @workitem['__output__'].should_not include("ERROR")
+      it 'should run rake with rvm and bundler' do
+        workitem['fields']['__error__'].should be_nil
+        workitem['__output__'].should include("rake, version")
+        workitem['__output__'].should_not include("ERROR")
+      end
     end      
-     
-    it 'should run rake using scm path w/o rvm & bundler' do
-      @workitem['fields']['tasks'] = '--version'
-      @workitem['fields'].delete('path')
-      @workitem['fields']['scm_path'] = @path
-      @workitem['fields']['use_rvm'] = false
-      @workitem['fields']['environment'] = 'CC=/usr/bin/gcc-4.2'
-      @workitem['fields']['use_bundle'] = false
-      @workitem['fields']['rake_executable'] = 'echo "fake rake, version"'
+    
+    context "when running rake using scm path w/o rvm & bundler" do
+      let(:fields) { super().merge({
+        'tasks' => '--version',
+        'path' => nil,
+        'scm_path' => path,
+        'use_rvm' => false,
+        'environment' => 'CC=/usr/bin/gcc-4.2',
+        'use_bundle' => false,
+        'rake_executable' => 'echo "fake rake, version"'
+      }) }
 
-      subject.perform(:execute, @workitem)
-
-      @workitem['fields']['__error__'].should be_nil
-      @workitem['__output__'].should include("rake, version")
-      @workitem['__output__'].should_not include("ERROR")
-     end
+      it 'should run rake using scm path w/o rvm & bundler' do
+        workitem['fields']['__error__'].should be_nil
+        workitem['__output__'].should include("rake, version")
+        workitem['__output__'].should_not include("ERROR")
+      end
+    end
   end
 
 end
